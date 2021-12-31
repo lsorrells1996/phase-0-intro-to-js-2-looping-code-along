8a84a124393e32610308195d724172dfffed7bd3
Lorenzo Sorrells <lsorrells1996@gmail.com>
2021-12-30 20:58:13 -0600

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solutions in this file
+
+
+const cards = [];
+
+function writeCards(name, event) {
+    for (let c = 0; c < name.length; c++) {
+        cards.push(`Thank you, ${name[c]}, for the wonderful ${event} gift!`);
+    }
+
+    return cards;
+}
+
+function countDown(number) {
+    let n = number;
+    while (n >= 0) {
+        console.log(n--);
+    }
+}
